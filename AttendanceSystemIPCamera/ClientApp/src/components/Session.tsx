dce5db2b0dcf74cb4b254fbfa279223500bad019
hanhmchau <hanhchaumn@gmail.com>
2020-03-16 21:54:52 +0700

    Fix breadcrumb routing

--- AttendanceSystemIPCamera/ClientApp/src/components/Session.tsx
+++ AttendanceSystemIPCamera/ClientApp/src/components/Session.tsx
@@ -232,7 +232,7 @@ class Session extends React.PureComponent<SessionProps, SessionLocalState> {
 					{
 						this.props.activeSession &&
 						<Breadcrumb.Item>
-							<Link to={`/groups/${this.props.activeSession.groupId}`}>
+							<Link to={`/group/${this.props.activeSession.groupId}`}>
 								<Icon type="hdd" />
 								<span>Group</span>
 							</Link>
