5cda517c958bd5803a4280fe79a12a7227921135
Duc Thanh <thanhndse@gmail.com>
2020-03-25 22:41:44 +0700

    merge 2 model to 1

--- RecognitionService/config/my_constant.py
+++ RecognitionService/config/my_constant.py
@@ -1,6 +1,5 @@
 from typing import Final
 embeddingsPath: Final = "output_dlib/embeddings.pickle"
-lePath: Final = "output_dlib/le.pickle"
-recognizerPath: Final = "output_dlib/recognizer.pickle"
 snapshotPath: Final = "snapshot/snapshot.jpg"
 vlcPath: Final = r'C:\Program Files\VideoLAN\VLC'
+recognizerModelPath: Final = "output_dlib/recognizer_model.pickle"
