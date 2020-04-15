aaa7c1c7f20b06b297885e52c1b9b848debaf45b
PhuND <dinhphu951998@gmail.com>
2020-04-15 12:05:41 +0700

    Update user view model

--- AttendanceSystemIPCamera/Framework/ViewModels/UserViewModel.cs
+++ AttendanceSystemIPCamera/Framework/ViewModels/UserViewModel.cs
@@ -10,7 +10,7 @@ namespace AttendanceSystemIPCamera.Framework.ViewModels
     {
         public string Code { get; set; }
         public string Name { get; set; }
-        public string Fullname { get; set; }
+        public string Email { get; set; }
         public string Image { get; set; }
     }
     public class UserAuthentication
@@ -30,7 +30,7 @@ namespace AttendanceSystemIPCamera.Framework.ViewModels
     {
         public string Code { get; set; }
         public string Name { get; set; }
-        public string Fullname { get; set; }
+        public string Email { get; set; }
         public string Image { get; set; }
     }
 
